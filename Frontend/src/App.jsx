ea79560e924e379f79ca03ed65be56d6104e9750
ShubhamThakur025 <thakur0805@gmail.com>
2024-09-03 18:08:11 +0530

    Basic ui setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css'
+import Task from './components/Task'
 
-function App(){
-  
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className='text-5xl font-bold ml-14'>Notes</h1>
+      <hr className='my-5'/>
+      <section className='ml-10 mt-8 flex flex-wrap justify-between items-center mx-auto'>
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </section>
+    </div>
+  )
 }
 
 export default App
---
+++ Frontend/src/components/Task.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Task() {
+  return (
+    <div className='p-5 bg-red-400 w-[350px] h-[300px] rounded-xl m-5'>
+      <h2 className='font-bold text-2xl'>Bath Daily</h2>
+      <p className='mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus in iste ea expedita quisquam quae obcaecati culpa minus. Eveniet temporibus nulla molestias iure distinctio neque sint excepturi quos iste rerum!</p>
+    </div>
+  )
+}
+
+export default Task
